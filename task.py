432b40d6270368cff6a44845bc6c0523ee95a9a9
dmcnall <38522211+dmcnall@users.noreply.github.com>
2020-03-01 21:18:14 -0800

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import math
+from datetime import date 
 
 
 def firstrun():
@@ -12,3 +13,9 @@ def computeArea(r):
 
 def firstLastList(newList):
     return newList[0], newList[-1]
+
+
+def computeDays(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
