41511fd60f56aa1b6e78f4417082a13c02455cd8
dmcnall <38522211+dmcnall@users.noreply.github.com>
2020-03-01 20:14:04 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,16 +4,13 @@ import task
 
 class TestCase(unittest.TestCase):
 
-
     def test1(self):
         expected = "success"
         self.assertEqual(expected, task.firstrun())
 
-
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
-
 
     def testComputeArea(self):
         self.assertEqual(task.computeArea(3), 3.14*3*3)
