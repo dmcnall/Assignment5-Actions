fbca6210ec3dc0df4284d5117664c68de59de7dc
dmcnall <38522211+dmcnall@users.noreply.github.com>
2020-03-01 20:05:11 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 import unittest
 import task
-import math 
 
 class TestCase(unittest.TestCase):
 
@@ -9,16 +8,14 @@ class TestCase(unittest.TestCase):
         expected = "success"
         self.assertEqual(expected, task.firstrun())
 
-
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
-
     def testComputeArea(self):
-        self.assertEqual(task.computeArea(3), math.pi*3*3)
+        self.assertEqual(task.computeArea(3), 3.14*3*3)
         self.assertEqual(task.computeArea(0), 0)
-        self.assertEqual(task.computeArea(1), math.pi*1*1)
+        self.assertEqual(task.computeArea(1), 3.14*1*1)
 
 
 if __name__ == '__main__':
